d0794075a6740291b53ffa95bede378fd974aeeb
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-20 23:19:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ function App() {
         <Routes>
 
           <Route element={<PrivateComponents />} >
-          {/* The components which we want to make private will be under this private component route The meaning of making component private means when user logged in only then this components will appear and user can access otherwise not*/}
+          {/* The components which we want to make private will be under this private component route The meaning of making component private means 
+          when user logged in only then this components will appear and user can access otherwise not*/}
           <Route path='/' element={<h1>Product Listing Component</h1>} />
           <Route path='/add' element={<h1>Add Product Listing Component</h1>} />
           <Route path='/update' element={<h1> update Product Listing Component</h1>} />
