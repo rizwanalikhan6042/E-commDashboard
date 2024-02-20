9c487e0efe992682e606da88aa6d03c7a909841e
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-20 23:18:54 +0530

    Update App.js
    The components which we want to make private will be under this private component route The meaning of making component private means when user logged in only then this components will appear and user can access otherwise not

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import SignUp from './components/SignUp';
-
+import PrivateComponents from './components/PrivateComponents';
+import Login from './components/Login';
 
 
 function App() {
@@ -15,18 +16,24 @@ function App() {
         <Navbar />
         
         <Routes>
-          <Route path='/' element={<h1>Product Listing Component</h1>} />
-         
 
+          <Route element={<PrivateComponents />} >
+          {/* The components which we want to make private will be under this private component route The meaning of making component private means when user logged in only then this components will appear and user can access otherwise not*/}
+          <Route path='/' element={<h1>Product Listing Component</h1>} />
           <Route path='/add' element={<h1>Add Product Listing Component</h1>} />
           <Route path='/update' element={<h1> update Product Listing Component</h1>} />
           <Route path='/logout' element={<h1>logout Product Listing Component</h1>} />
           <Route path='/profile' element={<h1>profile Product Listing Component</h1>} />
-          <Route path='/signup' element = {<SignUp/>}/>
+
+          {/* We are taking sign up page outside private component route because we don't want to make it private */}
           
-         
+          </Route>
+          {/*  Define a route for the '/signup' path and '/login' path, rendering the SignUp component and  Login component*/}
+          <Route path='/signup' element = {<SignUp/>}/>
+          <Route path='/login' element={<Login />} />
+
         </Routes>
-        {/* <h2>E-Dashboard</h2> */}
+        
         
       </BrowserRouter>
       <Footer />
