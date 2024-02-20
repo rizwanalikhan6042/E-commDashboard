9509426222251f618aaf0597ac19d86bfc5dbd28
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-20 19:40:56 +0530

    Update SignUp.js
    Local storage and other changes

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 // The useNavigate hook is used to programmatically navigate between different routes
 // in a React application. It provides a function that can be called to navigate to
 // a specific route, allowing for dynamic navigation based on application state or user actions.
@@ -15,45 +15,44 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const navigate = useNavigate();
 
-   
-   // Function to collect user data and initiate registration process
-const collectData = async () => {
-    // Log the user data to be collected (name, email, password)
-    console.log(name, email, password);
+    useEffect(()=>{
+        const auth = localStorage.getItem('user');
+        // if auth if is not null Or undefined it means user is logged in. 
+        // Then we will navigate to homepage and do not show the sign up page
+        if(auth){
+            navigate('/');
+        }
+    })
 
-    // Send a POST request to the server with user data for registration
-    let result = await fetch('http://localhost:3200/register', {
-        method: 'post',
-        body: JSON.stringify({ name, email, password }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-
-    // Parse the JSON response from the server
-    result = await result.json();
-    
-    // Log the registration result
-    console.log(result);
-
-    // If registration is successful, navigate the user to the home page
-    if (result) {
-        navigate('/');
+    // Define a function to collect and log user data when the SignUp button is clicked
+    const collectData =async () => {
+        console.log(name, email, password);
+        let result =await fetch('http://localhost:3200/register',{
+            method:'post',
+            body:JSON.stringify({name, email, password}),
+            headers:{
+                'Content-Type':'application/json'
+            },
+        });
+        result = await result.json();
+        console.log(result)
+        localStorage.setItem('user',JSON.stringify(result));
+        if(result){
+            navigate('/')
+        }
     }
-}
-
 
     // Render the SignUp form with input fields for name, email, and password
     return (
         <div className='register'>
             <h1>Register</h1>
-            {/* Input field for name ,email and password*/}
+            {/* Input field for name */}
             <input className="inputBox" type='text' placeholder='Enter name' value={name} onChange={(e) => { setName(e.target.value) }} />
 
-            
+            {/* Input field for email */}
             <input className="inputBox" value={email} onChange={(e) => { setEmail(e.target.value) }} type='text' placeholder='Enter email' />
 
-           
+            {/* Input field for password */}
             <input className="inputBox" value={password} onChange={(e) => { setPassword(e.target.value) }} type='password' placeholder='Enter password' />
 
             {/* SignUp button */}
