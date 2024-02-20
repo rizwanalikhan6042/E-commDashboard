8eb9656ae4e3ec114ba850a22276b656e6178695
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-20 19:38:22 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
 import React from "react";
-import  {Link}  from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const Navbar = () =>{
-return (
-    <div>
-        <ul className="nav-ul">
-           
-               <li><Link to="/">Product Component</Link></li>
-               <li><Link to="/Add">Add Product Component</Link></li>
-              <li> <Link to="/Update">update Product Component</Link></li>
-              <li> <Link to="/Logout">logout Component</Link></li>
-              <li> <Link to="/Profile">profile Component</Link></li>
-              <li> <Link to="/signup">signup Component</Link></li>
+const Navbar = () => {
+    const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () => {
+        localStorage.clear();
+        navigate('/signup');
+    }
 
-              
-        </ul>
-    </div>
-)
+    return (
+        <div>
+            <ul className="nav-ul">
+
+                <li><Link to="/">Product Component</Link></li>
+                <li><Link to="/Add">Add Product Component</Link></li>
+                <li> <Link to="/Update">update Product Component</Link></li>
+
+                <li> <Link to="/Profile">profile Component</Link></li>
+                {/* The feature of " if user is logged in , sign up button will not show and logout will show and if user is not logged in then sign up button will show ,logout will not show */}
+                <li> {auth ? <Link onClick={logout} to="/signup">logout</Link> :
+                    <Link to="/signup">signup</Link>}</li>
+                <li><Link to="/login">Login</Link> </li>
+
+
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
