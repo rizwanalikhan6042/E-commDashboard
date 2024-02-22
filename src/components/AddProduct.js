d0f6ac6fa8aa43d513b7b7b6d7373f5c2de51293
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-22 14:47:42 +0530

    Update AddProduct.js

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-const AddProduct = () =>{
+const AddProduct = () => {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [company, setCompany] = useState("");
+
+    const AddProduct = () => {
+        console.log(name, price, category, company);
+    }
+
+
+
 
     return (
-       <div>
-        <h2>Adding Products</h2>
-       </div>
+        <div className="product">
+            <h2>Add Product</h2>
+            <input className="inputBox" onChange={(e) => { setName(e.target.value) }} type="text" placeholder="Enter product name" />
+            <input className="inputBox" onChange={(e) => { setPrice(e.target.value) }} type="text" placeholder="Enter product price" />
+            <input className="inputBox" onChange={(e) => { setCategory(e.target.value) }} type="text" placeholder="Enter product category" />
+            <input className="inputBox" onChange={(e) => { setCompany(e.target.value) }} type="text" placeholder="Enter product company" />
+            <button className="Appbutton" onClick={AddProduct} >Add Product</button>
+
+        </div>
     )
 }
 export default AddProduct;
